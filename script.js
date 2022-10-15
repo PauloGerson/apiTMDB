f6ae12c0c0f0a220106da62fd04700624ba89c11
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-10-15 13:47:32 -0300

    firts commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const constainer = document.querySelector('.container');
+//console.log(constainer)
+const films  = [];
+function criarImg(imgs,spans){
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+    const divImg = document.createElement('div');
+    divImg.setAttribute('class','divImg')
+    img.setAttribute('src', imgs)
+    img.setAttribute('class', 'poster-img')
+    span.innerHTML = spans
+    divImg.append(img)
+    divImg.append(span)
+    constainer.append(divImg)
+    
+}
+
+
+const urlImg = `https://image.tmdb.org/t/p/w500`
+const apiKey = `cb4c98f66e1274d26140dd09a17f7a97`;
+const urlApi = fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`).then(resolve =>{
+    resolve.json().then(data => {
+        films.push(data.results)
+        films.map(el => {
+            console.log(el)
+            for(let img of el){
+                criarImg(`${urlImg}${img.poster_path}`, img.title)
+                
+            }
+        })
+        
+    })
+})
+
